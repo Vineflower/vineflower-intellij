a4b0730a2b6f82afefc700e930ef419690ae00ff
GitHub Action <action@github.com>
2021-11-15 00:11:08 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/earthcomputer/quiltflowerintellijplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.earthcomputer.quiltflowerintellijplugin.services
+
+import com.github.earthcomputer.quiltflowerintellijplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
